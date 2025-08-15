dcbf3a2720eb075537d821509f4d266d6eae2144
poptonydm <poptonydm@gmail.com>
2025-08-15 13:11:35 -0800

    login continuety(modified for form visibility js)

--- js/logins.js
+++ js/logins.js
@@ -3,12 +3,28 @@ const toLoginBtn = document.getElementById('to-loginbtn');
 const loginForm = document.getElementById('login');
 const registerForm = document.getElementById('register');
 
+loginForm.style.transition = 'opacity 1.5s';
+registerForm.style.transition = 'opacity 1.5s';
+
 toRegisterBtn.addEventListener('click', () => {
     loginForm.style.display = 'none';
+    setTimeout(() => {
+        loginForm.style.opacity = '0';
+    }, 10); // wait for the transition to complete
     registerForm.style.display = 'block';
+    setTimeout(() => {
+        registerForm.style.opacity = '1';
+    }, 0.1); // small delay to ensure display: block is applied
 });
 
 toLoginBtn.addEventListener('click', () => {
     registerForm.style.display = 'none';
+    setTimeout(() => {
+        registerForm.style.opacity = '0';
+    }, 10); // wait for the transition to complete
     loginForm.style.display = 'block';
+    loginForm.style.opacity = '0';
+    setTimeout(() => {
+        loginForm.style.opacity = '1';
+    }, 0.1); // small delay to ensure display: block is applied
 });
